847a41e1f318bad3f5f95bdba2eb80cb42f56c12
mohand55 <62512566+mohand55@users.noreply.github.com>
2020-12-30 14:10:12 +0200

    Add files via upload

--- MailHandler.py
+++ MailHandler.py
@@ -28,3 +28,33 @@ class MailHandler:
         #The body and the attachments for the mail
         self.message.attach(MIMEText(self.mailBody, 'plain'))
 
+    def SendMail(self):
+        if(self.mailBody == ""):
+            return "[ERROR] |: Mail Body cannot be left empty"
+        if(self.mailSubject == ""):
+            return "[ERROR] |: Please Insert a mail Subject"
+        if(self.receiverMail == ""):
+            return "[ERROR] |: There is No receiver Specified"
+        #Create SMTP session for sending the mail
+        #use gmail with port | yahoo with its port
+        if("gmail" in self.senderMail):
+            self.session = smtplib.SMTP('smtp.gmail.com', 587)
+        elif("yahoo" in self.senderMail):
+            #self.session = smtplib.SMTP('smtp.mail.yahoo.com', 465)
+            print("sorry Yahoo mailing is currently Down")
+            return "[FAILED] |: System Dosnt Support Mailing using a Yahoo STMP"
+        
+        #enable security
+        self.session.starttls() 
+
+        #login with mail_id and password
+        self.session.login(self.senderMail, self.senderPassword) 
+        self.text = self.message.as_string()
+        self.session.sendmail(self.senderMail, self.receiverMail, self.text)
+
+        #end session
+        self.session.quit()
+
+        #return success status
+        return "[SUCCESS] |: Mail Was Sent Successfully"
+
